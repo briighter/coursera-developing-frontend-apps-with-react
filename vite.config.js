c566007250553709064bb04785dd5c78fb84c66f
Jason Lima <jaylima0@gmail.com>
2024-12-08 18:46:47 -0500

    Init 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/coursera-developing-frontend-apps-with-react",
+    plugins: [react()],
 })
